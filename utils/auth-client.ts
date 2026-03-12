33d07a0d915c767f14caa7ac70431941095083d5
Aleksey Savin <aleksey.savin@protonmail.com>
2026-03-12 15:36:53 +1000

    fixed better-auth error

--- utils/auth-client.ts
+++ utils/auth-client.ts
@@ -4,7 +4,7 @@ import { ac, admin, user, manager } from 'utils/permissions'
 
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: 'http://localhost:3000',
+  baseURL: process.env.BETTER_AUTH_URL,
   plugins: [
     adminClient({
       ac,
